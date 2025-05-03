9a60cf5fafc48acb3c62a1e1853a343c0696f645
Vipin Yadav <76983380+yvipin6887@users.noreply.github.com>
2025-05-03 20:38:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-//your JS code here. If required.
+function domOutput(params) {
+  document.getElementById('output').innerText = params;
+}
+
+document.getElementById('btn').addEventListener('click', function () {
+  let inp = document.getElementById('ip').value;
+
+  let prom = new Promise((resolve) => {
+    setTimeout(() => resolve(inp), 2000); // ✅ You must call resolve(inp)
+  });
+
+  prom.then((num) => {
+    domOutput(num);
+	return new Promise((resolve) => {
+		setTimeout(() => resolve(num * 2), 2000)
+	});
+  }).then((num) => {
+	  domOutput(num);
+	  return new Promise((resolve) => {
+		setTimeout(() => resolve(num - 3), 1000)
+	});
+  }).then((num) => {
+	  domOutput(num);
+	  return new Promise((resolve) => {
+		setTimeout(() => resolve(num / 2), 2000)
+	});
+  }).then((num) => {
+	  domOutput(num);
+  });
+});
